ff7330ceceae641be85bf33ba22dd366e003857d
grenrock <stevengrenrock@live.com>
2025-06-24 13:34:33 -0700

    add css for mobile

--- components/header.js
+++ components/header.js
@@ -1,6 +1,7 @@
 const common = require("../lib/common");
-const header = async function () {
+const header = async function (session) {
   const params = common.params;
+  params.session = session;
   return await common.render(`components/header`, params);
 };
 
--- templates/publicPage.js
+++ templates/publicPage.js
@@ -1,10 +1,12 @@
 const footer = require("../components/footer");
 const head = require("../components/head");
+const header = require("../components/header");
 const common = require("../lib/common");
 
 const publicPage = async function (mainHtml) {
   const params = common.params;
   params.head = await head();
+  params.header = await header();
   params.main = mainHtml;
   params.footer = await footer();
 
