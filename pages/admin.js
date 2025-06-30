4a1c40a8951d58ccd5f0ee7170dc545f4c3be9d1
grenrock <stevengrenrock@live.com>
2025-06-30 13:17:52 -0700

    fix htmlAdminRows reference

--- pages/admin.js
+++ pages/admin.js
@@ -3,6 +3,7 @@ const common = require("../lib/common");
 const admin = async function (req, conn, session) {
   const params = common.params;
   params.session = session;
+  params.htmlAdminRows = "";
   return await common.render("pages/admin", params);
 };
 module.exports = admin;
